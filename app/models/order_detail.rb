fd7ce155462ac2e611795531b80002fbe6b9cf17
frosttkurr <thisiscyber13@gmail.com>
2022-04-20 14:33:37 +0000

    Append validation for order detail (nil for attributes)

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,2 +1,4 @@
 class OrderDetail < ApplicationRecord
+  validates :order_id, presence: true
+  validates :item_id, presence: true
 end
--- spec/models/order_detail_spec.rb
+++ spec/models/order_detail_spec.rb
@@ -4,4 +4,16 @@ RSpec.describe OrderDetail, type: :model do
   it 'has a valid order details' do
     expect(FactoryBot.build(:order_detail)).to be_valid
   end
+
+  it 'is invalid without order id' do
+    order_detail = FactoryBot.build(:order_detail, order_id: nil)
+    order_detail.valid?
+    expect(order_detail.errors[:order_id]).to include("can't be blank")
+  end
+  
+  it 'is invalid without item id' do
+    order_detail = FactoryBot.build(:order_detail, item_id: nil)
+    order_detail.valid?
+    expect(order_detail.errors[:item_id]).to include("can't be blank")
+  end
 end
