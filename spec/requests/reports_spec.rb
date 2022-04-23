409a2eb162245dfc558b50b01c8771783e53ca31
frosttkurr <thisiscyber13@gmail.com>
2022-04-23 03:18:17 +0000

    Add rspec for reports index

--- spec/requests/reports_spec.rb
+++ spec/requests/reports_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 RSpec.describe "Reports", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/reports/index"
+      Order.create(customer_email: "syakurr@generasigigih.com", total: 30000.0, status: "PAID")
+      Order.create(customer_email: "frostt@generasigigih.com", total: 30000.0, status: "NEW")
+      Order.create(customer_email: "annisa@generasigigih.com", total: 30000.0, status: "NEW")
+      get "/reports"
       expect(response).to have_http_status(:success)
     end
   end
